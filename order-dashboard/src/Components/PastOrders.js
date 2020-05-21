5cb4c544544295143f6a15cd4f6dda48a58e549d
aditijoshi7 <49837611+aditijoshi7@users.noreply.github.com>
2020-05-21 18:55:10 +0530

    Create PastOrders.js

---
+++ order-dashboard/src/Components/PastOrders.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'office-ui-fabric-react/dist/css/fabric.css';
+import 'C:/Users/Dell/Desktop/REACT/order-dashboard/src/App.css';
+
+import Navigation from './Navigation';
+import CardsSection from './CardsSection';
+import OperationsTable from './OperationsTable';
+
+const ActiveOrders= () =>{
+return(
+    
+    <div className="ms-Grid" dir="ltr">
+      <div className="ms-Grid-row">
+        <div className="ms-Grid-col ms-sm2 ms-xl2">
+          <Navigation />
+        </div>
+        <div className="main-element ms-Grid-col ms-sm10 ms-xl10">
+          <div className="ms-Grid-row">
+            <div className="mydiv" >
+                <h1>Past Orders</h1>
+            </div>
+          </div>
+          <div className="ms-Grid-row">
+            <OperationsTable />
+          </div>
+        </div>
+      </div>
+    </div>
+    
+);
+
+
+}
+
+export default ActiveOrders;
